b1fe0dc36d47d1c758bc4465c61c6cb024b594f3
Blaise Paradeza <z5160298@ad.unsw.edu.au>
2019-04-08 10:19:24 +1000

    imported bulma, updated navbar style

--- PHASE_2/Application_SourceCode/frontend/src/components/Navigation.vue
+++ PHASE_2/Application_SourceCode/frontend/src/components/Navigation.vue
@@ -1,9 +1,19 @@
 <template>
   <div>
-    <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/search">Search</router-link>
-      <router-link to="/graphs">Graphs</router-link>
+    <nav class="navbar is-light">
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item">
+            <router-link to="/">Home</router-link>
+          </a>
+          <a class="navbar-item">
+            <router-link to="/search">Search</router-link>
+          </a>
+          <a class="navbar-item">
+            <router-link to="/graphs">Graphs</router-link>
+          </a>
+        </div>
+      </div>
     </nav>
   </div>
 </template>
