bb8152ab783a016d3529393b3cb74580c281ce76
[JemZzz] <jemmaz1997@gmail.com>
2019-04-22 18:50:13 +1000

    removed nav bar

--- vue-dashboard-master/src/components/admin/app-navbar/AppNavbar.vue
+++ vue-dashboard-master/src/components/admin/app-navbar/AppNavbar.vue
@@ -4,16 +4,6 @@
       <img class="logo" src="../../../assets/icons/vue-logo.png"/>
     </span>
 
-    <menu-dropdown
-      v-for="(page, id) in pages"
-      :key="id"
-      :options="page.options"
-      :name="page.name"
-      :logo="page.logo"
-      :link="page.link"
-      class="col-xs-1"
-    ></menu-dropdown>
-
     <profile-dropdown slot="profile" class="col-xs-1 nav-item" v-if="username">
       <img src="https://png2.kisspng.com/sh/a5baf9ff32dc32d008e8ce4cb23a230b/L0KzQYm3VsI3N6Z8i5H0aYP2gLBuTfF3aaVmip9Ac3X1PbT2jgB2fJZ3Rdtsb372PcT2hwR4aaNqRdZudnXvf8Hskr02amQ3T9VsOXPmQYbtV745P2M8SqkDMEG4Q4G3U8U1OGI9S6g3cH7q/kisspng-avatar-user-computer-icons-software-developer-5b327cc9cc15f7.872727801530035401836.png"/>
     </profile-dropdown>
@@ -111,14 +101,6 @@ export default {
   },
   data () {
     return {
-      pages: [
-        {
-          name: this.$t('Results'),
-          link: 'collapse',
-          logo: 'vuestic-icon-ui-elements',
-          options: []
-        }
-      ],
       username:""
     }
   },
